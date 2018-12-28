3737ec44ffe48347516c9bb4661c4f3a1a06997f
TimThuma <30806431+TimThuma@users.noreply.github.com>
2018-12-28 15:53:19 +0100

    Update

--- components/Layout.js
+++ components/Layout.js
@@ -12,11 +12,11 @@ export default props => {
                     rel="stylesheet"
                     href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
                 />
+                <title>CrowdCoin</title>
             </Head>
 
             <Header />
             <Favicon url="https://raw.githubusercontent.com/TimThuma/crowdcoin10/master/static/favicon.ico" />
-
             {props.children}
         </Container>
     );
