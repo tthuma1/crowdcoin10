2ea5400f7c4364355f897205ddf5ddb2adb420a0
TimThuma <30806431+TimThuma@users.noreply.github.com>
2018-12-29 15:16:52 +0100

    Update

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,10 @@ import { Container } from 'semantic-ui-react';
 import Head from 'next/head';
 import Header from './Header';
 
-let i = 0;
 let https = require('https');
 setInterval(function() {
     https.get('https://crowdcoin10.herokuapp.com/');
-    console.log('ping pong ' + i++);
-}, 10000); // 1500000 ms = 25 minutes
+}, 1740000); // 28 minutes
 
 export default props => {
     return (
