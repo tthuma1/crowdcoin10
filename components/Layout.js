02351e1ef63faddf9aaa3e86dacbfe42befdbc10
TimThuma <30806431+TimThuma@users.noreply.github.com>
2019-07-22 16:22:54 +0200

    Update

--- components/Layout.js
+++ components/Layout.js
@@ -17,10 +17,6 @@ export default props => {
                     type="image/ico"
                     href="../static/favicon.ico"
                 />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
             </Head>
 
             <Header />
